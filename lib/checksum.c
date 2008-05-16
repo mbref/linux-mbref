38399b778a8730a7238be0489c605eb78f0d4bc7
Michal Simek <monstr@monstr.eu>
2008-05-16 14:14:05 +0200

    add checksum to lib and header to asm-generic

--- lib/checksum.c
+++ lib/checksum.c
@@ -1,8 +1,4 @@
 /*
- * arch/microblaze/lib/checksum.c
- *
- * Copyright (C) 2008 Michal Simek <monstr@monstr.eu>
- *
  * INET		An implementation of the TCP/IP protocol suite for the LINUX
  *		operating system.  INET is implemented using the  BSD Socket
  *		interface as the means of communication with the user level.
