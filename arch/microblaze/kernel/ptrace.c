49b04bf880f83cfd7bdb13d13d07b932fd4173ff
Michal Simek <monstr@monstr.eu>
2008-07-08 11:08:31 +0200

    Clean ptrace for generic use

--- arch/microblaze/kernel/ptrace.c
+++ arch/microblaze/kernel/ptrace.c
@@ -71,36 +71,15 @@ static microblaze_reg_t *reg_save_addr(unsigned reg_offs,
 
 long arch_ptrace(struct task_struct *child, long request, long addr, long data)
 {
-	int rval;
+	int ret;
+	unsigned long val = 0;
 
 	switch (request) {
-		unsigned long val, copied;
-
-	case PTRACE_PEEKTEXT: /* read word at location addr. */
-	case PTRACE_PEEKDATA:
-		pr_debug("PEEKTEXT/PEEKDATA at %08lX\n", addr);
-		copied = access_process_vm(child, addr, &val, sizeof(val), 0);
-		rval = -EIO;
-		if (copied != sizeof(val))
-			break;
-		rval = put_user(val, (unsigned long *)data);
-		goto out;
-
-	case PTRACE_POKETEXT: /* write the word at location addr. */
-	case PTRACE_POKEDATA:
-		pr_debug("POKETEXT/POKEDATA to %08lX\n", addr);
-		rval = 0;
-		if (access_process_vm(child, addr, &data, sizeof(data), 1)
-			== sizeof(data))
-			break;
-		rval = -EIO;
-		goto out;
-
 	/* Read/write the word at location ADDR in the registers. */
 	case PTRACE_PEEKUSR:
 	case PTRACE_POKEUSR:
 		pr_debug("PEEKUSR/POKEUSR : 0x%08lx\n", addr);
-		rval = 0;
+		ret = 0;
 		if (addr >= PT_SIZE && request == PTRACE_PEEKUSR) {
 			/* Special requests that don't actually correspond
 			 to offsets in struct pt_regs. */
@@ -112,7 +91,7 @@ long arch_ptrace(struct task_struct *child, long request, long addr, long data)
 				val = child->mm->end_code
 					- child->mm->start_code;
 			} else {
-				rval = -EIO;
+				ret = -EIO;
 			}
 		} else if (addr >= 0 && addr < PT_SIZE && (addr & 0x3) == 0) {
 			microblaze_reg_t *reg_addr = reg_save_addr(addr, child);
@@ -121,60 +100,17 @@ long arch_ptrace(struct task_struct *child, long request, long addr, long data)
 			else
 				*reg_addr = data;
 		} else
-			rval = -EIO;
+			ret = -EIO;
 
-		if (rval == 0 && request == PTRACE_PEEKUSR)
-			rval = put_user(val, (unsigned long *)data);
+		if (ret == 0 && request == PTRACE_PEEKUSR)
+			ret = put_user(val, (unsigned long *)data);
 		goto out;
-
-	/* Continue and stop at next (return from) syscall */
-	case PTRACE_SYSCALL:
-		pr_debug("PTRACE_SYSCALL\n");
-	case PTRACE_SINGLESTEP:
-		pr_debug("PTRACE_SINGLESTEP\n");
-	/* Restart after a signal. */
-	case PTRACE_CONT:
-		pr_debug("PTRACE_CONT\n");
-		rval = -EIO;
-		if (!valid_signal(data))
-			break;
-
-		if (request == PTRACE_SYSCALL)
-			set_tsk_thread_flag(child, TIF_SYSCALL_TRACE);
-		else
-			clear_tsk_thread_flag(child, TIF_SYSCALL_TRACE);
-
-		child->exit_code = data;
-		pr_debug("wakeup_process\n");
-		wake_up_process(child);
-		rval = 0;
-		break;
-
-	/*
-	 * make the child exit. Best I can do is send it a sigkill.
-	 * perhaps it should be put in the status that it wants to
-	 * exit.
-	 */
-	case PTRACE_KILL:
-		pr_debug("PTRACE_KILL\n");
-		rval = 0;
-		if (child->exit_state == EXIT_ZOMBIE)	/* already dead */
-			break;
-		child->exit_code = SIGKILL;
-		wake_up_process(child);
-		break;
-
-	case PTRACE_DETACH: /* detach a process that was attached. */
-		pr_debug("PTRACE_DETACH\n");
-		rval = ptrace_detach(child, data);
-		break;
-
 	default:
-		rval = -EIO;
-		goto out;
+		ret = ptrace_request(child, request, addr, data);
+		break;
 	}
  out:
-	return rval;
+	return ret;
 }
 
 #if 0
