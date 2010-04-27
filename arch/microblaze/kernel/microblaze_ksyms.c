cbad8bc5a27420aea59fc8726c4761e6fca663fe
Steven J. Magnani <steve@digidescorp.com>
2010-04-27 13:00:35 -0500

    microblaze: export assembly functions used by modules
    Modules that use copy_{to,from}_user(), memcpy(), and memset() fail to build
    in certain circumstances.
    
    Signed-off-by: Steven J. Magnani <steve@digidescorp.com>
    Signed-off-by: Michal Simek <monstr@monstr.eu>

--- arch/microblaze/kernel/microblaze_ksyms.c
+++ arch/microblaze/kernel/microblaze_ksyms.c
@@ -60,3 +60,13 @@ EXPORT_SYMBOL(__copy_tofrom_user);
 extern void _mcount(void);
 EXPORT_SYMBOL(_mcount);
 #endif
+
+/*
+ * Assembly functions that may be used (directly or indirectly) by modules
+ */
+EXPORT_SYMBOL(__copy_tofrom_user);
+
+#ifdef CONFIG_OPT_LIB_ASM
+EXPORT_SYMBOL(memcpy);
+EXPORT_SYMBOL(memmove);
+#endif
