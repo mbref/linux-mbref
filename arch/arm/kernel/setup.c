bb2eb855a4b19094a648685189ebaf3fee303887
Jason Wu <jason.wu@petalogix.com>
2011-09-08 09:29:35 +1000

    zynq: boot external dtb by default
    Boot the built-in dtb if external dtb address is not provided.
    
    DTB boot order:
     * External dtb
     * built-in dtb
    
    Signed-off-by: Jason Wu <jason.wu@petalogix.com>

--- arch/arm/kernel/setup.c
+++ arch/arm/kernel/setup.c
@@ -874,7 +874,8 @@ void __init setup_arch(char **cmdline_p)
 	struct machine_desc *mdesc;
 #ifdef CONFIG_ARCH_ZYNQ
 	extern u32 _fdt_start[];
-	__atags_pointer = virt_to_phys(_fdt_start);
+	 if (!__atags_pointer)
+		__atags_pointer = virt_to_phys(_fdt_start);
 #endif
 
 	unwind_init();
